e4717ff07f0e5ee55e4a9042a7b1da31bf3e6b5d
Andrey <matradze@gmail.com>
2017-11-08 19:08:35 +0700

    worktest

---
+++ js/main - копия.js
@@ -0,0 +1,63 @@
+/*console.log("hello frontender");
+var string =  "hi gays, \ngg wp, \ngh hf,\n\t afk";
+console.log(string.charAt(string.length -2));
+var tempp3;
+console.log(tempp3);
+
+function  sdfdsfdsdsad(fsdfsdfsd) {
+    return "gg" + fsdfsdfsd;
+}
+console.log(sdfdsfdsdsad("wp"));
+
+if (false) console.log('4')
+
+var name = "Asya" , homesity;
+
+if (name === "Oleg") {
+	homesity = "Tomsk";
+} else if (name === "Zahar") {
+	homesity = "NSK";
+} else if ( name === "Andrey") {
+	homesity = "Yurga";
+}
+console.log(homesity);
+
+switch (name) {
+	case "Zahar": homesity = "NSK";
+	case "Oleg": homesity = "Tomsk"; 
+	case "Andrey": homesity = "Yurga";
+	default: homesity = "LA"; 
+}
+
+console.log(homesity);
+
+
+*/
+
+/*console.log("5"-3)
+
+console.log("5"+3)
+
+console.log("5"+-'3')
+*/
+
+
+function GetMessage() {
+    alert("Свободен!!11")
+
+}
+
+function Vremz() {
+    var timeInda = Math.round(Date.now()/1000);
+    if (timeInda > 1509553202) {
+        GetMessage();
+    } console.log(timeInda);
+}
+
+Vremz();
+
+//console.log(parseFloat("3.14"));
+console.log(parseFloat("314e-2"));
+console.log(parseFloat("0.0314E+2"));
+console.log(parseFloat("3.14какие-нибудь не цифровые знаки"));
+console.log(parseFloat("3.14"));
---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+
+
+var greet = function() {
+    return "dada " + this.name;
+};
+
+var person = {
+    name2: 'Olof',
+    red: greet
+};
+/*
+console.log(person.red('dadada'));
+
+
+function Person(name, age, year) { this.year = age;}
+
+*/
+
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit);
+console.log("D don't me tach and go \taway ");
+console.log("D don't me tach and go \naway ");
+console.log("The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.");
+
+
+console.log("green"=="Green");
+console.log("green">"blue");
+console.log( "borsh");
+
+var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
+
+var haiku = "Blowing from the wes\nFallen leaves gather\nIn the east.";
+    console.log(haiku);
+var signedIn;
+console.log(signedIn);
+console.log("Hello" % 10);
+console.log("3" > 1);
+console.log(1 !== false);
+
+var thingOne = "red";
+var thingTwo = "blue";
+
+console.log(thingOne+thingTwo);
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill / 0.15;
+var total = tip;
+
+console.log(total);
